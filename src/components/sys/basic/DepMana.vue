e093a7f11c5dfce42bbd7070e6dc1a09db2a5e88
theLuckyCode <362753059@qq.com>
2020-02-21 17:19:49 +0800

    画完部门以及管理员操作的一半

--- src/components/sys/basic/DepMana.vue
+++ src/components/sys/basic/DepMana.vue
@@ -34,6 +34,29 @@
         </span>
       </span>
         </el-tree>
+        <!--   增加部门时候的弹框     -->
+        <el-dialog
+                title="添加部门"
+                :visible.sync="dialogVisible"
+                width="30%"
+                :before-close="handleClose">
+            <div>
+                <table>
+                    <tr>
+                        <td><el-tag>上级部门</el-tag></td>
+                        <td><span>{{pname}}</span></td>
+                    </tr>
+                    <tr>
+                        <td><el-tag>部门名称</el-tag></td>
+                        <td><el-input v-model="dep.name" placeholder="请输入部门名称..." ></el-input></td>
+                    </tr>
+                </table>
+            </div>
+            <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="doAddDep">确 定</el-button>
+  </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -47,7 +70,13 @@
                 defaultProps: {
                     children: 'children',
                     label: 'name'
-                }
+                },
+                dialogVisible:false,
+                dep:{
+                    name:'',
+                    parentId:-1
+                },
+                pname:''  //父项的名字
             }
         },
         mounted() {
@@ -62,11 +91,43 @@
             }
         },
         methods:{
+            doAddDep(){
+                this.postRequest("/system/basic/department/",this.dep).then(resp=>{
+                    if (resp){
+                        this.dep={
+                            name:'',
+                            parentId:-1
+                        };
+                        this.pname='' ;
+                        this.initDeps();
+                        this.dialogVisible=false;
+                    }
+                })
+            },
             showAddDep(data){
-              alert(JSON.stringify(data));
+              // alert(JSON.stringify(data));
+              this.pname = data.name;
+              this.dep.parentId = data.id;
+              this.dialogVisible=true;
             },
             deleteDep(data){
-              alert(JSON.stringify(data));
+                //alert(JSON.stringify(data));
+                    this.$confirm('此操作将永久删除【'+data.name+'】角色, 是否继续?', '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    }).then(() => {
+                        this.deleteRequest("/system/basic/department/"+data.id).then(resp=>{
+                            if (resp){
+                                this.initDeps();
+                            }
+                        })
+                    }).catch(() => {
+                        this.$message({
+                            type: 'info',
+                            message: '已取消删除'
+                        });
+                    });
             },
             initDeps(){
              this.getRequest("/system/basic/department/").then(resp=>{
--- src/views/sys/SysHr.vue
+++ src/views/sys/SysHr.vue
@@ -1,13 +1,88 @@
 <template>
-    <h2>操作员管理</h2>
+    <div>
+        <div style="margin-top: 10px;display: flex;justify-content:center">
+        <!--display 属性规定元素应该生成的框的类型,flex 属性用于设置或检索弹性盒模型对象的子元素如何分配空间-->
+        <!--justify-content 用于设置或检索弹性盒子元素在主轴（横轴）方向上的对齐方式-->
+            <el-input prefix-icon="el-icon-search" v-model="keywords" placeholder="通过用户名搜索用户..." style="width: 400px;margin-right: 10px"></el-input>
+            <el-button icon="el-icon-search" size="small" type="primary">搜索</el-button>
+        </div>
+        <div style="margin-top: 50px;display: flex;flex-wrap: wrap;justify-content: space-around">
+            <el-card class="box-card" v-for="(hr,index) in hrs" :key="index">
+                <div slot="header" class="clearfix">
+                    <span>{{hr.name}}</span>
+                    <el-button style="float: right; padding: 3px 0;color: red" type="text" icon="el-icon-delete"></el-button>
+                </div>
+                <div>
+                    <div style="display: flex;justify-content: center">
+                        <img :src="hr.userface" :alt="hr.name" :title="hr.name" style="width: 72px;height: 72px;border-radius: 72px;">
+                    </div>
+                    <div style="margin-top: 20px;font-size: 12px;color: #2c90ff">
+                            <div>用户名：{{hr.name}}</div>
+                            <div>手机号码：{{hr.phone}}</div>
+                            <div>电话号码：{{hr.telephone}}</div>
+                            <div>地址：{{hr.address}}</div>
+                            <div>用户状态：<el-switch
+                                    v-model="hr.enabled"
+                                    active-text="启用"
+                                    inactive-text="禁用"
+                                    @change="enabledChange(hr)">
+                            </el-switch></div>
+                            <div>用户角色：<el-tag type="success" v-for="(role,indexj) in hr.roles" :key="indexj" style="margin-right: 4px">{{role.nameZh}}
+                                          </el-tag>
+                                          <el-popover
+                                             placement="bottom"
+                                             title="标题"
+                                             width="200"
+                                             trigger="click"
+                                             content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+                                             <el-button slot="reference" type="text" icon="el-icon-more"></el-button>
+                                          </el-popover>
+                            </div>
+                            <div >备注：{{hr.remark}}</div>
+                    </div>
+                </div>
+            </el-card>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "SysHr"
+        name: "SysHr",
+        data(){
+            return {
+                keywords:'',
+                hrs:[]
+            }
+        },
+        mounted() {
+            this.initHrs();
+        },
+        methods:{
+            initHrs(){
+                this.getRequest("/system/hr/").then(resp=>{
+                    if (resp){
+                        this.hrs = resp;
+                    }
+                })
+            },
+            enabledChange(hr){
+                delete hr.roles;  //删除roles属性，不然后端报错
+                this.putRequest("/system/hr/",hr).then(resp=>{
+                    if (resp){
+                         this.initHrs();
+                    }
+                })
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style>
+
+    .box-card{
+       width: 350px;
+       margin-bottom: 20px;
+    }
 
 </style>
